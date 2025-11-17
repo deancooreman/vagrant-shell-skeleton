a214a7bd9aa27c5053c27c5d1bb6949ae049036e
Dean Cooreman <dean.cooreman@student.hogent.be>
2025-11-17 20:26:23 +0100

    Exercise - Reproducible virtual environments with vagrant

---
+++ provisioning/fedora.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+#
+# Provisioning script for srv001
+
+#------------------------------------------------------------------------------
+# Bash settings
+#------------------------------------------------------------------------------
+
+# Enable "Bash strict mode"
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't mask errors in piped commands
+
+#------------------------------------------------------------------------------
+# Variables
+#------------------------------------------------------------------------------
+
+# Location of provisioning scripts and files
+readonly PROVISIONING_SCRIPTS="/vagrant/provisioning/"
+# Location of files to be copied to this server
+readonly PROVISIONING_FILES="${PROVISIONING_SCRIPTS}/files/${HOSTNAME}"
+
+export PROVISIONING_SCRIPTS PROVISIONING_FILES
+
+#------------------------------------------------------------------------------
+# "Imports"
+#------------------------------------------------------------------------------
+
+# Utility functions
+source ${PROVISIONING_SCRIPTS}/util.sh
+# Actions/settings common to all servers
+source ${PROVISIONING_SCRIPTS}/common.sh
+
+#------------------------------------------------------------------------------
+# Provision server
+#------------------------------------------------------------------------------
+
+log "Starting server specific provisioning tasks on ${HOSTNAME}"
+
+# TODO: insert code here, e.g. install Apache, add users (see the provided
+# functions in utils.sh), etc.
